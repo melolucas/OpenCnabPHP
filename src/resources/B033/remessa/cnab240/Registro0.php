480c057acbf3269d8e224d0082ec62e7afa58997
Lucas Melo <lucasmelo2597@gmail.com>
2022-04-27 15:55:31 -0300

    Ajuste codigo_transmissao

--- src/resources/B033/remessa/cnab240/Registro0.php
+++ src/resources/B033/remessa/cnab240/Registro0.php
@@ -67,28 +67,34 @@ class Registro0 extends Generico0
             'tipo' => 'int',
             'required' => true
         ),
-        'agencia' => array(
-            'tamanho' => 4,
-            'default' => '',
-            'tipo' => 'int',
-            'required' => true
-        ),
-        'filler12' => array(
-            'tamanho' => 4,
+        // 'agencia' => array(
+        //     'tamanho' => 4,
+        //     'default' => '',
+        //     'tipo' => 'int',
+        //     'required' => true
+        // ),
+        // 'filler12' => array(
+        //     'tamanho' => 4,
+        //     'default' => '0',
+        //     'tipo' => 'int',
+        //     'required' => true
+        // ),
+        // 'codigo_beneficiario' => array(
+        //     'tamanho' => 6,
+        //     'default' => '0',
+        //     'tipo' => 'int',
+        //     'required' => true
+        // ),
+        // 'codigo_beneficiario_dv' => array(
+        //     'tamanho' => 1,
+        //     'default' => '0',
+        //     'tipo' => 'int',
+        //     'required' => true
+        // ),
+        'codigo_transmissao' => array(
+            'tamanho' => 15,
             'default' => '0',
-            'tipo' => 'int',
-            'required' => true
-        ),
-        'codigo_beneficiario' => array(
-            'tamanho' => 6,
-            'default' => '0',
-            'tipo' => 'int',
-            'required' => true
-        ),
-        'codigo_beneficiario_dv' => array(
-            'tamanho' => 1,
-            'default' => '0',
-            'tipo' => 'int',
+            'tipo' => 'alfa',
             'required' => true
         ),
         'filler2' => array(
